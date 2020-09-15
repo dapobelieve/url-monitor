99759c6d67f9c2db342e293faaecb0c97bb13c07
Dapo Michaels <dapomichaels@gmail.com>
2020-09-16 00:29:02 +0100

    clean up index.js and include workers

--- index.js
+++ index.js
@@ -1,121 +1,23 @@
 /**
- * Main file
+ * Primary file for API
  */
-
-//Dependencies
-
-var http = require("http");
-var https = require("https");
-var url  = require("url");
-var stringDecoder = require("string_decoder").StringDecoder;
-var config = require('./lib/config');
-var fs = require('fs');
-var handlers = require('./lib/handlers');
-var helpers = require('./lib/helpers');
-// 4158375309
-
-helpers.sendTwilioSms('07069494803', 'You are the best ever', function(res) {
-	console.log(res);
-});
-
-//start/instantiating the HTTP server and have it listen on port:3000
-var httpServer = http.createServer(function (req, res) {
-	unifiedServer(req, res);
-});
-
-httpServer.listen(config.httpPort, function() {
-	console.log("Listening on port "+config.httpPort+" in "+config.envName+"...");
-});
+var server = require("./lib/server");
+var workers = require("./lib/workers");
 
 
+var app = {}
 
-// Instantiate the HTTPS server
-var httpsServerOptions = {
-	'key': fs.readFileSync('./https/key.pem'),
-	'cert': fs.readFileSync('./https/cert.pem')
-}
+app.init = function () {
+	// Start Server
+	server.init()
 
+	// Start Workers
+	workers.init()
 
-var httpsServer = https.createServer(httpsServerOptions, function (res, req) {
-	unifiedServer(req, res);
-});
-
-
-// Instantiate the HTTPS server
-httpsServer.listen(config.httpsPort, function() {
-	console.log("Listening on port "+config.httpsPort+" in "+config.envName+"...");
-});
-
-
-
-var unifiedServer = function (req, res) {
-
-	console.log(req.url);
-	// get and parse url
-	var parsedUrl = url.parse(req.url, true);
-
-	// get the path of the url
-	var path =  parsedUrl.pathname;
-	var trimmedPath = path.replace(/^\/+|\/+$/g, '');
-
-	// get the query strings
-	var queryStringObject = parsedUrl.query;
-
-	//Get Http Method
-	var method = req.method.toLowerCase();
-
-	//get headers as an Object
-	var headers = req.headers
-
-
-	// parse payload if they exist
-	var decoder = new stringDecoder('utf-8');
-	var buffer = '';
-
-	req.on('data', function (data) {
-		buffer += decoder.write(data);
-	});
-
-	req.on('end', function() {
-		buffer += decoder.end();
-
-		//choose handler request should goto
-		var chosenHandler = typeof(router[trimmedPath]) !== 'undefined' ? router[trimmedPath] : handlers.notFound;
-
-		//construct the data object to send to the handler
-		var data = {
-			'trimmedPath': trimmedPath,
-			'queryStringObject': queryStringObject,
-			'method': method,
-			'headers': headers,
-			'payload': helpers.parseJsonToObject(buffer)
-		}
-
-		//route the request to the handler specified in the router
-		chosenHandler(data, function(statusCode, payload = {}) {
-			// use status code sent by the handler or default to 200
-			statusCode = typeof(statusCode) == 'number' ? statusCode : 200;
-
-			//use payload sent by the handler or default to empty object
-			payload = typeof(payload) == 'object' ? JSON.stringify(payload) : {};
-
-			//return response
-			res.setHeader('Content-Type', 'application/json');
-			res.writeHead(statusCode);
-			res.end(payload);
-
-			console.log(statusCode, payload);
-
-		})
-	});	
 };
 
+//Execute,turn on the lights
+app.init()
 
-//define the router
-var router = {
-	'ping': handlers.ping,
-	'users': handlers.users,
-	'tokens':  handlers.tokens,
-	'checks': handlers.checks
-}
 
+module.exports = app
